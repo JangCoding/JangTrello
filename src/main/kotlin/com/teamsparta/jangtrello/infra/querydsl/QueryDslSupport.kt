424cd347984bc68d0f0ca6b10f9d33fab1e9fa6d
codingjang <jjangga813@naver.com>
2024-02-05 19:54:04 +0900

    QueryDslSupport 주석 추가

--- src/main/kotlin/com/teamsparta/jangtrello/infra/querydsl/QueryDslSupport.kt
+++ src/main/kotlin/com/teamsparta/jangtrello/infra/querydsl/QueryDslSupport.kt
@@ -11,10 +11,12 @@ abstract class QueryDslSupport {
     // 이를 통해 엔터티 매니저를 사용하여 영속성 컨텍스트를 관리하고, 데이터베이스와의 상호 작용을 수행할 수 있음
     // 영속성 컨텍스트를 객체에 제공하여 JPA 에서 제공하는 기능을 사용할 수 있게 한다
     @PersistenceContext
-    protected lateinit var entityManager: EntityManager
+    private lateinit var entityManager: EntityManager
 
+    // 각 레포지토리에서 생성자 주입으로 사용할 수 있지만 그러면 각 객체마다 별개의 팩토리가 생성됨.
+    // 하나만 만들어서 전체를 관리하도록 하는 것.
     protected val queryFactory : JPAQueryFactory
-        get(){
+        get(){  // 매번 새로 생성되서 사용하게됨. 한번 생성해서 매번 비우는 것 보다 효율적
             return JPAQueryFactory(entityManager)
         }
 }
